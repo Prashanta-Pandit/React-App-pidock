a4b6954fed2c981aff8cb195b70867ac2767c518
Prashanta Pandit <prasant@Prashantas-MacBook-Pro.local>
2024-05-28 17:55:55 +1000

    refine the pages

--- src/Components/Nav.jsx
+++ src/Components/Nav.jsx
@@ -3,8 +3,8 @@ import React from 'react';
 export default function Navbar (){
     return (
         <>
-        <div className="relative w-full bg-white">
-        <div className="mx-auto flex max-w-7xl items-center justify-between px-4 py-2 sm:px-6 lg:px-8">
+        <div className="fixed top-0 w-full bg-gray-200 shadow-md">
+         <div className="mx-auto flex max-w-7xl items-center justify-between px-1 py-2 sm:px-6 lg:px-8">
             <div className="inline-flex items-center space-x-2">
                 <span>
                     <img
@@ -62,7 +62,7 @@ export default function Navbar (){
                 <line x1="4" y1="18" x2="20" y2="18"></line>
               </svg>
             </div>
-        </div>
+         </div>
         </div>
         </>
     )
