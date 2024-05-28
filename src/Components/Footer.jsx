0ba2475ab8960324c5f977794c6e0f02f164ed6e
Prashanta Pandit <prasant@Prashantas-MacBook-Pro.local>
2024-05-28 16:27:49 +1000

    update a title and Registration page.

--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -5,7 +5,7 @@ export default function Footer(){
     <>
     <footer className="fixed bottom-0 left-0 w-full z-50 bg-black ">
           <div className="text-center py-5">
-            <span className="ml-4 text-lg font-normal text-white ">magpies.online | All right reserve</span>
+            <span className="ml-4 text-lg font-normal text-white ">magpies.online | All right reserved</span>
           </div>
      </footer>
     </>
