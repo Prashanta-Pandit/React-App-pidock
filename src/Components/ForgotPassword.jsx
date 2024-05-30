67b07655c3a973441bb19df7b0b4526de3467d3b
Prashanta Pandit <prasant@Prashantas-MacBook-Pro.local>
2024-05-31 08:58:32 +1000

    change the password form title

--- src/Components/ForgotPassword.jsx
+++ src/Components/ForgotPassword.jsx
@@ -36,7 +36,7 @@ export default function ForgotPassword(){
         <>
         <div className=" mt-10 flex min-h-full flex-col justify-center px-6 py-12 lg:px-8">
         <div className="sm:mx-auto sm:w-full sm:max-w-sm">
-            <h2 className="mt-10 text-center text-2xl font-bold leading-9 tracking-tight text-gray-900">Forgot password</h2>
+            <h2 className="mt-10 text-center text-2xl font-bold leading-9 tracking-tight text-gray-900">Reset password</h2>
         </div>
         <div className="mt-10 sm:mx-auto sm:w-full sm:max-w-sm">
         {/* handle the onsubmit on form. */}
