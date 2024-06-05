e3c82a9ca78b28500632bd1c2dd67d3123658b65
Prashanta Pandit <prasant@Prashantas-MacBook-Pro.local>
2024-06-05 19:52:11 +1000

    get the data in placeholder to edit, apply conditional rendering

--- src/Components/Portal/EditUserDetails.jsx
+++ src/Components/Portal/EditUserDetails.jsx
@@ -8,6 +8,7 @@ export default function EditUserDetails() {
     const [signedInUserId, setSignedInUserId] = useState('');
     const [userDetails, setUserDetails] = useState([]);
     const [isCancelButtonClicked, setIsCancelButtonClicked] = useState(false);
+    const [showInput, setShowInput] = useState(false); // Add state to track input visibility in the form when user try to edit.
 
     useEffect(() => {
         const userId = localStorage.getItem('signedInUserUid'); // this is user id that is stored after signing in from signin page.
@@ -27,7 +28,6 @@ export default function EditUserDetails() {
         }
     }, [signedInUserId]); // Re-run this effect when signedInUserId changes
 
-    
     function handleEditUserDetails(e) {
         e.preventDefault(); // stop the page reload when clicked
     }
@@ -35,6 +35,11 @@ export default function EditUserDetails() {
     function handleCancel(e) {
         e.preventDefault(); // Handle cancel action
         setIsCancelButtonClicked(true);
+    }
+    
+    //if the user click the form inputs.
+    function handleInputClick() {
+        setShowInput(true); // Show input when paragraph is clicked
     }
 
     return (
@@ -45,6 +50,7 @@ export default function EditUserDetails() {
                 <form className="space-y-6" onSubmit={handleEditUserDetails}>
                     <div className="px-4 sm:px-0">
                         <h3 className="text-base font-bold leading-7 text-gray-900">Update your details</h3>
+                        <p className='text-sm font-light text-gray-600 pl-6'><span className='text-red-600'>* </span>Click to edit</p>
                     </div>
                     <div className="mt-6 border-t border-gray-100">
                         <dl className="divide-y divide-gray-100">
@@ -54,10 +60,15 @@ export default function EditUserDetails() {
                                     <span className="ml-2">First Name</span>
                                 </dt>
                                 <dd className="mt-1 text-sm leading-6 text-gray-700 sm:col-span-2 sm:mt-0">
-                                    <input 
-                                        className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6" 
-                                        placeholder={userDetails.length > 0 ? `${userDetails[0].firstName}` : "loading..."}
-                                    />
+                                    {/*conditional render when the user click input field.*/}
+                                    {showInput ? (
+                                        <input 
+                                            className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6" 
+                                            placeholder={userDetails.length > 0 ? `${userDetails[0].firstName}` : "loading..."}
+                                        />
+                                    ) : (
+                                        <p onClick={handleInputClick}>{userDetails.length > 0 ? `${userDetails[0].firstName}` : "loading..."}</p>
+                                    )}
                                 </dd>
                             </div>
                             <div className="px-4 py-6 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-0">
@@ -66,24 +77,35 @@ export default function EditUserDetails() {
                                     <span className="ml-2">Last Name</span>
                                 </dt>
                                 <dd className="mt-1 text-sm leading-6 text-gray-700 sm:col-span-2 sm:mt-0">
-                                    <input 
-                                        className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6" 
-                                        placeholder={userDetails.length > 0 ? `${userDetails[0].lastName}` : "loading..."}
-                                    />
+                                    {/*conditional render when the user click input field.*/}
+                                    {showInput ? (
+                                        <input 
+                                            className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6" 
+                                            placeholder={userDetails.length > 0 ? `${userDetails[0].lastName}` : "loading..."}
+                                        />
+                                    ) : (
+                                        <p onClick={handleInputClick}>{userDetails.length > 0 ? `${userDetails[0].lastName}` : "loading..."}</p>
+                                    )}
                                 </dd>
                             </div>
                             <div className="px-4 py-6 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-0">
                                 <dt className="flex flex-row text-sm font-bold leading-6 text-gray-900">
                                     <Mail />
-                                    <span className="ml-2">Email address</span>
+                                    <span className="ml-2">Email</span>
                                 </dt>
                                 <dd className="mt-1 text-sm leading-6 text-gray-700 sm:col-span-2 sm:mt-0">
-                                    <input 
-                                        className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6" 
-                                        placeholder={userDetails.length > 0 ? `${userDetails[0].email}` : "loading..."}
-                                    />
+                                    {/*conditional render when the user click input field.*/}
+                                    {showInput ? (
+                                        <input 
+                                            className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6" 
+                                            placeholder={userDetails.length > 0 ? `${userDetails[0].email}` : "loading..."}
+                                        />
+                                    ) : (
+                                        <p onClick={handleInputClick}>{userDetails.length > 0 ? `${userDetails[0].email}` : "loading..."}</p>
+                                    )}
                                 </dd>
                             </div>
+                            {/* Other fields */}
                             <div className="px-4 py-6 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-0">
                                 <div className="sm:col-span-2"></div> 
                                 <dd className="flex flex-col sm:flex-row sm:space-x-4 justify-end text-sm font-normal leading-6">
@@ -108,3 +130,4 @@ export default function EditUserDetails() {
         )
     );
 }
+
