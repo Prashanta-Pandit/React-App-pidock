6952e88417b5fc82e4e50a86d6328c41d857e707
Prashanta Pandit <prasant@Prashantas-MacBook-Pro.local>
2024-06-09 19:29:49 +1000

    Chat and Help Support added

--- src/Components/Help.jsx
+++ src/Components/Help.jsx
@@ -48,7 +48,7 @@ export default function Help() {
                                 <form className="space-y-6" onSubmit={handleSubmit}>
                                     <div>
                                         <div className="flex items-center justify-between">
-                                            <label htmlFor="email" className="block text-sm font-medium leading-6 text-gray-900">Name</label>
+                                            <label htmlFor="name" className="block text-sm font-medium leading-6 text-gray-900">Name</label>
                                         </div>
                                         <div className="mt-2">
                                             <input type="text" required value={name} onChange={(e) => setName(e.target.value)} className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-black sm:text-sm sm:leading-6"/>
@@ -64,7 +64,7 @@ export default function Help() {
                                     </div>
                                     <div>
                                         <div className="flex items-center justify-between">
-                                            <label htmlFor="email" className="block text-sm font-medium leading-6 text-gray-900">Phone</label>
+                                            <label htmlFor="phone" className="block text-sm font-medium leading-6 text-gray-900">Phone</label>
                                         </div>
                                         <div className="mt-2">
                                             <input  type="text" required value={phone} onChange={(e) => setPhone(e.target.value)} className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-black sm:text-sm sm:leading-6"/>
@@ -72,7 +72,7 @@ export default function Help() {
                                     </div>
                                     <div>
                                         <div className="flex items-center justify-between">
-                                            <label htmlFor="email" className="block text-sm font-medium leading-6 text-gray-900">Message</label>
+                                            <label htmlFor="message" required className="block text-sm font-medium leading-6 text-gray-900">Message</label>
                                         </div>
                                         <div className="mt-2">
                                             <textarea type="text" required value={message} onChange={(e) => setMessage(e.target.value)} className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-black sm:text-sm sm:leading-6">
