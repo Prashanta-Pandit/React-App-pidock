95300354f5730639c18643626fb12ee5b51d6694
Prashanta Pandit <prashanta927pandit@gmail.com>
2024-07-01 22:23:49 +1000

    test

--- functions/index.js
+++ functions/index.js
@@ -5,3 +5,7 @@ exports.randomNumber = functions.https.onRequest((request, response) => {
   const number = Math.round(Math.random() * 100);
   response.send(number.toString());
 });
+
+exports.toMagpies = functions.https.onRequest((request, response)=>{
+  response.redirect('https://magpies.online/');
+});
