35a476a1665077952265c516c40fe776a17dc54f
Prashanta Pandit <prashanta927pandit@gmail.com>
2024-07-01 22:22:27 +1000

    test

--- functions/index.js
+++ functions/index.js
@@ -1,11 +1,7 @@
 const functions = require("firebase-functions");
 
-// Firebase function can be called in two different ways:
-// 1. HTTP request and 2. HTTP callable
-
 // HTTP request example
 exports.randomNumber = functions.https.onRequest((request, response) => {
   const number = Math.round(Math.random() * 100);
   response.send(number.toString());
 });
-
